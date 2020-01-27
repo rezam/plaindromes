d62295f97fe130175f8ad509e507cd3cacc9915b
Reza Masoumpour <reza.mas88@gmail.com>
2020-01-27 21:10:19 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+inputText = document.querySelector(".input");
+runBtn = document.querySelector(".run");
+resultLbl = document.querySelector(".result");
+
+function plainRun() {
+  let reverseNum = inputText.value.length - 1;
+  let goodText = inputText.value.toLowerCase().replace(/ /g, "");
+  let resultArr = "";
+
+  for (index = 0; reverseNum >= index; reverseNum--) {
+    resultArr += goodText.substr(reverseNum, 1);
+  }
+
+  if(resultArr === goodText) {
+    resultLbl.innerHTML = "True";
+  } else {
+    resultLbl.innerHTML = "False";
+  }
+
+  alert(goodText);
+  alert(resultArr);
+}
+
+runBtn.addEventListener("click", plainRun);
